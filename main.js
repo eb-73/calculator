b5d00b64cd6b0de8ca78ae7eab2d442513b02105
eb-73 <ebrahim.baghbani73@gmail.com>
2021-07-09 14:05:19 +0430

    add 2 flag to solve backspace problem

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ let value="";
 let history=[];
 let s=""; 
 operator.innerHTML="AC";
-let flag=true;
+let flag1=true;
+let flag2=false;
 const calculate=function(btn){
      let content=btn.innerHTML;
     
@@ -51,6 +52,7 @@ const calculate=function(btn){
             h.innerHTML=eval(value);
             history.push(value);
             operator.innerHTML="=";
+            flag2=true;
         }
         break;
         case "AC":{
@@ -58,27 +60,34 @@ const calculate=function(btn){
             operator.innerHTML="AC";
             h.innerHTML=" ";
             his.innerHTML=" ";
-            flag=true;
+            flag1=true;
         }
         break;
         case "C":{
             let con="";
-        if(h.innerHTML!=" "){
+        if(h.innerHTML!=" "&&!flag2){
             con=h.innerHTML;
             h.innerHTML=con.slice(0,(con.length)-1);
             value=value.slice(0,(value.length)-1);
              
         }
+        else if (h.innerHTML!=" "&&flag2){
+            con=h.innerHTML;
+            h.innerHTML=con.slice(0,(con.length)-1);
+            const evall=eval(value).toString();
+            value=evall.slice(0,(evall.length)-1);
+            flag2=false;
+        }
         }
         break;
         case "HST":{  
-            if (flag){
+            if (flag1){
                 history.map(x=>{ 
                     s=`${x.toString()}=${eval(x)}</br>`;
                     his.innerHTML+=s;
                 });
                 
-                flag=false;
+                flag1=false;
             }
           
             
